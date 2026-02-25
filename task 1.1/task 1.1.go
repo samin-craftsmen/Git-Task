d2e09a5367645ab160300a4e2a18d9e4f4112aa1
samin-craftsmen <samin.yasar@craftsmensoftware.com>
2026-02-25 07:49:10 +0600

    Add CLI menu display And implement menu interaction loop And Added notes file

--- task 1.1/task 1.1.go
+++ task 1.1/task 1.1.go
@@ -37,3 +37,34 @@ func deleteTask() {
 	tasks = append(tasks[:index-1], tasks[index:]...)
 	fmt.Println("Task deleted!")
 }
+
+func showMenu() {
+	fmt.Println("\n1. Add Task")
+	fmt.Println("2. View Tasks")
+	fmt.Println("3. Delete Task")
+	fmt.Println("4. Exit")
+}
+
+func menu() {
+	for {
+		showMenu()
+
+		var choice int
+		fmt.Print("Choose: ")
+		fmt.Scan(&choice)
+
+		switch choice {
+		case 1:
+			addTask()
+		case 2:
+			viewTasks()
+		case 3:
+			deleteTask()
+		case 4:
+			fmt.Println("Bye!")
+			return
+		default:
+			fmt.Println("Invalid option")
+		}
+	}
+}
