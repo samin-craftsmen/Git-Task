2e4fc7c384652cd8badfc25ad8c423f090558a04
samin-craftsmen <samin.yasar@craftsmensoftware.com>
2026-02-25 07:56:19 +0600

    Add reset functionality

--- task 1.2/task 1.2.go
+++ task 1.2/task 1.2.go
@@ -8,3 +8,13 @@ func increment() {
 	counter++
 	fmt.Println("Counter:", counter)
 }
+
+func decrement() {
+	counter--
+	fmt.Println("Counter:", counter)
+}
+
+func reset() {
+	counter = 0
+	fmt.Println("Counter reset to 0")
+}
