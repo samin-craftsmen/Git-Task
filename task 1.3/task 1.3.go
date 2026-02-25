37c856ba891ad5fb62903e7ee386d1f028f3ebd8
samin-craftsmen <samin.yasar@craftsmensoftware.com>
2026-02-25 08:12:02 +0600

    Add Fahrenheit to Celsius conversion function And add menu display function

--- task 1.3/task 1.3.go
+++ task 1.3/task 1.3.go
@@ -1,5 +1,17 @@
 package task_1_3
 
+import "fmt"
+
 func cToF(c float64) float64 {
 	return (c*9/5 + 32)
+}
+
+func fToC(f float64) float64 {
+	return (f - 32) * 5 / 9
+}
+
+func showMenu() {
+	fmt.Println("\n1. Celsius to Fahrenheit")
+	fmt.Println("2. Fahrenheit to Celsius")
+	fmt.Println("3. Exit")
 }
