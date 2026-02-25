e03370011d7c3d9d994cf33f43b17d38cd4d590d
samin-craftsmen <samin.yasar@craftsmensoftware.com>
2026-02-25 07:56:19 +0600

    Add reset functionality And add menu display

--- task 1.2/task 1.2.go
+++ task 1.2/task 1.2.go
@@ -8,3 +8,20 @@ func increment() {
 	counter++
 	fmt.Println("Counter:", counter)
 }
+
+func decrement() {
+	counter--
+	fmt.Println("Counter:", counter)
+}
+
+func reset() {
+	counter = 0
+	fmt.Println("Counter reset to 0")
+}
+
+func showMenu() {
+	fmt.Println("\n1. Increment")
+	fmt.Println("2. Decrement")
+	fmt.Println("3. Reset")
+	fmt.Println("4. Exit")
+}
